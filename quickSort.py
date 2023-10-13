ae183a8df4b268dde559935a777e61c71cfeb5e0
Ibrahim-Chaibedraa <chaibedraa.mohamedibrahim@univ-tiaret.dz>
2023-10-13 21:41:15 +0100

    double check

--- quickSort.py
+++ quickSort.py
@@ -1,3 +1,7 @@
+import numpy
+import time
+import sys
+sys.setrecursionlimit(200000)
 def partition(arr, start, end):
     pivot = arr[end]
     partIndex = start
@@ -15,3 +19,22 @@ def quickSort(arr, start, end):
         quickSort(arr, start, partIndex-1)
         quickSort(arr, partIndex+1, end)
     return arr
+
+
+best = numpy.load('100thousand.npy')
+worst = numpy.load('100thousandReverse.npy')
+avg = numpy.load('100ThousandRand.npy')
+
+startBest = time.time()
+quickSort(best, 0, 99999)
+endBest = time.time()
+startWorst = time.time()
+quickSort(worst, 0, 99999)
+endWorst = time.time()
+startAvg = time.time()
+quickSort(avg, 0, 99999)
+endAvg = time.time()
+
+print(f'worst case: {endWorst-startWorst}s')
+print(f'avg case: {endAvg - startAvg}s')
+print(f'best case: {endBest - startBest}s')
