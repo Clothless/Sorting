62779b5eb284238ddd2571e2c9819428f66b995c
issamHosni <issam.hosni@univ-tiaret.dz>
2023-10-13 19:10:05 +0100

    done with my part

--- mergeSort.py
+++ mergeSort.py
@@ -1,3 +1,11 @@
+import numpy as np
+import time as t
+import math
+
+best = np.load('100thousand.npy')
+worst = np.load('100thousandReverse.npy')
+avg = np.load('100ThousandRand.npy')
+
 def merge(left, right, arr):
     i = 0
     j = 0
@@ -31,3 +39,17 @@ def mergeSort(arr):
         mergeSort(right)
         merge(left, right, arr)
     return arr
+
+startBest = t.time()
+print(mergeSort(best))
+endBest = t.time()
+startAvg = t.time()
+print(mergeSort(avg))
+endAvg = t.time()
+startWorst = t.time()
+print(mergeSort(worst))
+endWorst = t.time()
+
+print(f'worst case: {round(endWorst-startWorst, 3)}s')
+print(f'avg case: {round(endAvg - startAvg, 3)}s')
+print(f'best case: {round(endBest - startBest, 3)}s')
